1bf22965a6f4cf1763e0baf9f6e412ed275619db
Bjarni R. Einarsson <bre@klaki.net>
2014-09-17 18:05:08 +0100

    MVP user database CGI helpers

---
+++ user-mv.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+#
+# This script will act as a cgi-bin script, accepting POST requests of the
+# form:
+#
+#   oldjson=JSON filename (slashes disallowed)
+#   newjson=JSON filename (slashes disallowed)
+#
+# If the named JSON file exists, it will be renamed to use the new name.
+#
+import cgi
+import os
+import json
+import sys
+
+JSON_HOME = '/tmp/json-data/'
+
+try:
+    assert(os.getenv('REQUEST_METHOD') == 'POST')
+
+    request = cgi.FieldStorage()
+    json_old = str(request['oldjson'].value)
+    json_new = str(request['newjson'].value)
+    assert('/' not in json_old)
+    assert('/' not in json_new)
+
+    json_old_path = os.path.join(JSON_HOME, json_old)
+    json_new_path = os.path.join(JSON_HOME, json_new)
+
+    assert(os.exists(json_old_path))
+    assert(not os.exists(json_new_path))
+    os.rename(json_old_path, json_new_path)
+
+    print 'Content-type: application/json'
+    print
+    print '%s' % open(json_new_path).read()
+
+except:
+    import traceback
+    sys.stderr.write(traceback.format_exc())
+    print 'Content-type: text/plain'
+    print 'Status: 500'
+    print
+    print 'Internal Scripting Fudge'
---
+++ user-up.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+#
+# This script will act as a cgi-bin script, accepting POST requests of the
+# form:
+#
+#   json=JSON filename (slashes disallowed)
+#   variable=<something>
+#   value=<something>
+#
+# If the named JSON file exists, it will be edited to update the named
+# variable with the requested value.  It will then return the contents of
+# the updated JSON file as output.
+#
+import cgi
+import os
+import json
+import sys
+
+JSON_HOME = '/tmp/json-data/'
+
+try:
+    assert(os.getenv('REQUEST_METHOD') == 'POST')
+
+    request = cgi.FieldStorage()
+    json_file = str(request['json'].value)
+    variable = unicode(request['variable'].value)
+    value = unicode(request['value'].value)
+
+    assert('/' not in json_file)
+    json_path = os.path.join(JSON_HOME, json_file)
+
+    data = json.load(open(json_path, 'r'))
+    data[variable] = value
+    json.dump(data, open(json_path, 'w'))
+
+    print 'Content-type: application/json'
+    print
+    print '%s' % data
+
+except:
+    import traceback
+    sys.stderr.write(traceback.format_exc())
+    print 'Content-type: text/plain'
+    print 'Status: 500'
+    print
+    print 'Internal Scripting Fudge'
