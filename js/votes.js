d1a8cd2b90b2aca40900bb3d492744f80d0a6203
Smári McCarthy <smari@mailpile.is>
2014-09-17 19:17:20 +0100

    Votes now somewhat meaningful

--- js/votes.js
+++ js/votes.js
@@ -1,6 +1,6 @@
 
 votes = {
-	userinfo: null,
+	userinfo: {},
 
 	init: function() {
 		// Initialize all the things
@@ -8,16 +8,23 @@ votes = {
 		if ($.cookie("token")) {
 			// Refresh the cookie
 			$.cookie("token", $.cookie("token"), {expires: 10});
+			$(".vote").show();
 		}
 
 		$("#login-button").click(function() {
 			var user = $("#login-username").val();
 			var pass = $("#login-password").val();
-			votes.login(user, pass)
-;		});
+			votes.login(user, pass);
+		});
 
 		$("#login-button").click(function() {
 			votes.logout();
+		});
+
+		$(".vote").click(function(e) {
+			var issue = $(e.target).data("issue");
+			var value = $(e.target).data("value");
+			votes.cast_vote(issue, value);
 		});
 
 	},
@@ -32,6 +39,7 @@ votes = {
 				votes.userinfo = data;
 				$.cookie("username", username);
 				$.cookie("token", token, {expires: 10});
+				$(".vote").show();
 				console.log("Logged in.");
 			} else {
 				votes.warning("Invalid user name or password");
@@ -46,21 +54,22 @@ votes = {
 	},
 
 	cast_vote: function(issue, vote) {
-		userinfo["vote."+ issue] = vote;
+		console.log("Casting vote of " + vote + " to issue " + issue);
 		votes.update_path("vote." + issue, vote);
 	},
 
 	update_path: function(key, value) {
-		$.getJSON("https://www.mailpile.is/cgi-bin/mailpile/user-up.py", {key: value}, function() {
+		votes.userinfo[key] = value;
+		$.getJSON("https://www.mailpile.is/cgi-bin/mailpile/user-up.py", {key: value}, function(data) {
 			// pass
 		});
-	}
+	},
 
 	set_path: function(username, password) {
 		$.getJSON("https://www.mailpile.is/cgi-bin/mailpile/user-mv.py", {}, function() {
 
 		});
-	}
+	},
 
 
 	warning: function(msg) {
