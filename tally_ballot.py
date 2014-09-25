711348e955e0a48062a8a119fa1f23421b25b989
Smári McCarthy <smari@mailpile.is>
2014-09-25 14:57:31 +0100

    Support for counting ranked election results using Shulze method.

---
+++ tally_ballot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from pyvotecore.schulze_pr import SchulzePR
+import os
+import json
+
+JSON_HOME = 'db/' # '/home/mailpile/hoipoi/db/'
+electionid = 1
+
+# Collect all ballots from user JSON files
+ballots = []
+for json_file in os.listdir(JSON_HOME):
+    if ".json" not in json_file:
+        continue
+    json_path = os.path.join(JSON_HOME, json_file)
+    data = json.load(open(json_path, 'r'))
+    ballot = data["election.%d" % (electionid)].split(",")
+    ballots.append(ballot)
+
+# Tally ballots
+tally = []
+for ballot in ballots:
+    done = False
+    for t in tally:
+        if t["ballot"] == ballot:
+            t["count"] += 0
+            done = True
+    if not done:
+        tally.append({"count": 1, "ballot": ballot})
+
+# Calculate result
+result = SchulzePR(tally, ballot_notation = "grouping").as_dict()
+
+print result["order"]
